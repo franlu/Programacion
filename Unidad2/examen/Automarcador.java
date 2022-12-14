816bcf78d8664671dc2165f69dde0ddd711fb5c1
Fran Lucena <fran.lucena@gmail.com>
2022-12-14 16:54:46 +0100

    UP Solucion Ej1

--- Unidad2/examen/Automarcador.java
+++ Unidad2/examen/Automarcador.java
@@ -9,16 +9,19 @@ import bpc.daw.objetos.MarcadorBaloncesto;
 public class Automarcador {
 
     public static void main(String[] args) {
-        
+
         String nLocal = "Estudiantes";
         String nVisitante = "CB Granada";
-        char opcion = ' ';
-        String puntos;
-        int ptos;
-        Scanner scan = new Scanner(System.in);
-        
+        String aux;
+        char opcion;
+        boolean partidoEnJuego = true;
+        int puntos;
+        Scanner leerTeclado = new Scanner(System.in);
+
+        // Declarar e inicializar el objeto MarcadorBaloncesto
         MarcadorBaloncesto marcador = new MarcadorBaloncesto(nLocal, nVisitante);
-        
+
+        // Menu Inicial
         System.out.println(
                 "Marcador automático entre equipos. Local: " + marcador.getNombreLocal()
                 + "; Visitante: " + marcador.getNombreVisitante());
@@ -28,48 +31,81 @@ public class Automarcador {
                 "Pulsa la letra 'S' para visualizar el marcador final y terminar el partido.");
         System.out.println("1) indique el equipo anotador: 'L' (local) o 'V' (visitante)");
         System.out.println("2) Marque la puntuación anotada: (1,2,3) + INTRO");
-        
-        
-        while (opcion != 'S') {
-            
+
+
+        while (partidoEnJuego) { // opcion == 'S'
+
             System.out.println(
                     "a) indique equipo anotador (L|V), S (salir), P (puntos actuales):");
-            opcion = scan.next().charAt(0);
-            
-            switch (opcion){
-                case 'L':
-                    System.out.println("b) indique la puntuación de la canasta");
-                    puntos = scan.next();
-                    ptos = Integer.parseInt(puntos);
-                    marcador.anotarCanasta(opcion, ptos);
+
+            // almacenar el primer caracter que pulsa el usuario
+            opcion = leerTeclado.next().charAt(0);
+
+
+            if (opcion == 'L' || opcion == 'V') {
+
+                System.out.println("b) indique la puntuación de la canasta");
+                aux = leerTeclado.next();
+                puntos = Integer.parseInt(aux);
+
+                while (puntos < 1 || puntos > 3) {
+
+                    System.out.println("ERROR: Puntuación no válida");
+                    System.out.println("Pulse una tecla válida: 1, 2 o 3.");
+                    aux = leerTeclado.next();
+                    puntos = Integer.parseInt(aux);
+
+                }
+
+                marcador.anotarCanasta(opcion, puntos);
+
+                if (opcion == 'L') {
+
                     System.out.println(
-                            "Marca Local con una canasta de "
-                            + puntos + "puntos.");
-                    break;
-                case 'V':
-                    System.out.println("b) indique la puntuación de la canasta");
-                    puntos = scan.next();
-                    ptos = Integer.parseInt(puntos);
-                    marcador.anotarCanasta(opcion, ptos);
+                    "Marca LOCAL con una canasta de "
+                            + puntos + " puntos.");
+                } else {
+
                     System.out.println(
-                            "Marca Visitante con una canasta de " 
-                            + puntos + "puntos.");
-                    break;
-                case 'P':
-                    System.out.println("En estos momentos:");
-                    System.out.println(marcador.toString());
+                    "Marca VISITANTE con una canasta de "
+                            + puntos + " puntos.");
+                }
+
+            } else if (opcion == 'P') {
+
+                System.out.println("En estos momentos:");
+                System.out.println(marcador.toString());
+
+                if (marcador.getPuntosLocal() > marcador.getPuntosVisitante()){
+
                     System.out.println(""
-                            + "Va ganando el equipo: " 
-                            + marcador.getNombreEquipoGanador());
-                    break;
-                case 'S':
-                    System.out.println("Ha pulsado salir del marcador.");
-                    System.out.println("Fin del partido. Marcador final:");
-                    System.out.println(marcador.toString());
-                    break;
-                default:
-                    System.out.println("Opción Incorrecta.");
-            }  
+                        + "Va ganando el equipo LOCAL: "
+                        + marcador.getNombreEquipoGanador());
+
+                } else if (marcador.getPuntosLocal() < marcador.getPuntosVisitante()) {
+
+                    System.out.println(""
+                        + "Va ganando el equipo VISITANTE: "
+                        + marcador.getNombreEquipoGanador());
+                } else {
+                    // Si el partido esta en EMPATE getNombreEquipoGanado devuelve null
+                    System.out.println("El partido se encuentra en EMPATE");
+
+                }
+
+            } else if (opcion == 'S') {
+
+                System.out.println("Ha pulsado salir del marcador.");
+                System.out.println("Fin del partido. Marcador final:");
+                System.out.println(marcador.toString());
+                partidoEnJuego = false;
+
+            } else {
+
+                System.out.println("ERROR: Opción Incorrecta");
+                System.out.println("Pulse una tecla válida (L, V, P o S)");
+
+            }
         }
     }
 }

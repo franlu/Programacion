d142298f3520c9dd657a5ba5d191ee8f1426b8b4
Fran Lucena <fran.lucena@gmail.com>
2023-01-19 11:37:00 +0100

    FIX tabulacion

--- Unidad3/Ejemplos/Ej04Arrays.java
+++ Unidad3/Ejemplos/Ej04Arrays.java
@@ -10,22 +10,22 @@ public class Ej04Arrays {
 		canciones.add("Salta - King África");
 		canciones.add("La mané - Cantajuegos");
 		
-    System.out.println("Recorriendo 1ª forma");
-		
-    for (String cancion : canciones) {
-			System.out.println(cancion);
+		System.out.println("Recorriendo 1ª forma");
+
+		for (String cancion : canciones) {
+				System.out.println(cancion);
+			}
+
+		System.out.println("Recorriendo 2ª forma");
+
+		for (int x = 0; x < canciones.size(); x++) {
+				System.out.println(canciones.get(x));
+			}
+
+		System.out.println("Recorriendo 3ª forma");
+
+			canciones.forEach((cancion) -> {
+				System.out.println(cancion);
+			});
 		}
-		
-    System.out.println("Recorriendo 2ª forma");
-		
-    for (int x = 0; x < canciones.size(); x++) {
-			System.out.println(canciones.get(x));
-		}
-		
-    System.out.println("Recorriendo 3ª forma");
-		
-		canciones.forEach((cancion) -> {
-			System.out.println(cancion);
-		});
-	}
 }

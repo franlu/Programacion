a6a608d962bf771510b74fffefe45e4662cd22ef
Fran Lucena <fran.lucena@gmail.com>
2023-02-17 19:50:21 +0100

    Update AnimalAcuatico.java

--- Unidad4/Ejemplos/AnimalAcuatico.java
+++ Unidad4/Ejemplos/AnimalAcuatico.java
@@ -5,3 +5,12 @@ public interface Animal {
 public interface AnimalAcuatico extends Animal {
     void nadar();
 }
+
+public class Fish implements AquaticAnimal {
+    public void mover() {
+        System.out.println("El pez se esta moviendo.");
+    }
+    public void nadar() {
+        System.out.println("El pez esta nadando hacia fondo del mar.");
+    }
+}

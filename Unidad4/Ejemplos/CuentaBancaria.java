1e002e910b13fea0edd25e8c2467c06911a17250
Fran Lucena <fran.lucena@gmail.com>
2023-02-17 17:01:09 +0100

    Update CuentaBancaria.java

--- Unidad4/Ejemplos/CuentaBancaria.java
+++ Unidad4/Ejemplos/CuentaBancaria.java
@@ -1,5 +1,6 @@
 /**
-* La clase representa una cuenta bancaria.
+* La clase representa una cuenta bancaria 
+* que almacena una máximo de 10000 €
 * 
 * @author Fran <fran.lucena@gmail.com>
 */
@@ -7,14 +8,14 @@
 public class CuentaBancaria {
     private int saldo;
 
-    public void depositar(int cantidad) {
+    public void depositar(int cantidad) throws IllegalArgumentException {
         if (saldo + cantidad > 10000) {
-            throw new IllegalStateException("No se puede depositar más de $10,000 pesos.");
+            throw new IllegalStateException("No se puede depositar más de 10.000 €.");
         }
         saldo += cantidad;
     }
 
-    public void retirar(int cantidad) {
+    public void retirar(int cantidad) throws IllegalArgumentException {
         if (saldo - cantidad < 0) {
             throw new IllegalStateException("No se puede retirar más de lo que hay en la cuenta.");
         }

08a44b8f509969e0a91aaba1cbb3156facbf9141
franlu <fran.lucena@gmail.com>
2023-03-19 11:39:55 +0100

    ADD ejemplo de uso de la clase Coche

---
+++ Unidad5/Practicas/practica01/Principal.java
@@ -0,0 +1,42 @@
+package practica01;
+
+import java.util.List;
+import java.util.ArrayList;
+
+// Al estar las clases en el mismo paquete no es necesario importarlas
+
+/**
+ * Ejemplo de uso de las clase Coche, Procesador y ProcesadoException
+ * 
+ * @author Fran <fran.lucena@gmail.com>
+ * @since 19/03/2023
+ * @version 1.0
+ * @see Clase Coche, Procesador y ProcesadorException
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        Coche c1 = new Coche("ALHAMBRA", "Rojo", 5, "SEAT");
+        Coche c2 = new Coche("C2", "Verde", 3, "CITROEN");
+        Coche c3 = new Coche("MOKKA-e", "Azul", 5, "OPEL");
+        
+        List<Object> objetos = new ArrayList();
+        
+        
+        // Coche
+        System.out.println(c1.arrancar());
+        System.out.println(c1.acelerar());
+        System.out.println(c1.frenar());
+        System.out.println(c1.parar());
+        System.out.println(c1); // llamada a toString
+        
+        objetos.add(120);
+        objetos.add("Esto es una cadena de texto");
+        objetos.add(c2);
+        objetos.add(Math.PI); // double, para provocar la excepcion
+        
+        
+    }
+    
+}

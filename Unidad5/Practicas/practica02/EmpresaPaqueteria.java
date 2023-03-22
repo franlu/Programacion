9c9388bf083a5a32cbd31fe47f8146c0fb0fc414
franlu <fran.lucena@gmail.com>
2023-03-22 08:42:39 +0100

    ADD Métodos de EmpresaPaqueteria

--- Unidad5/Practicas/practica02/EmpresaPaqueteria.java
+++ Unidad5/Practicas/practica02/EmpresaPaqueteria.java
@@ -1,5 +1,34 @@
 package examen;
 
+import java.util.List;
+
+/**
+ * Define los requisitos que tiene que tener una clase para ser considerada
+ * una empresa de paquetería.
+ * 
+ * @author Fran <fran.lucena@gmail.com>
+ * @since 21/03/2023
+ * @version 1.0
+ * @see Paquete, Transportista
+ */
 public interface EmpresaPaqueteria {
     
+    /**
+     * Aceptar pedido y asignar un repartidor
+     * 
+     * @param p el paquete que va a ser registrado 
+     */
+    public void registrarPedido(Paquete p);
+    
+    /**
+     * Consulta los transportista que trabajan para la empresa.
+     * 
+     * @return List lista de transportistas
+     */
+    public List<Transportista> getTransportistas();
+    
+    /**
+     *  Enviar paquetes a su destino.
+     */
+    default void enviarPaquetes(){}
 }
